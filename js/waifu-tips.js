af7176c86d011d1182aedbd6a8d77ca068305b70
19047471 <19047471@cnsuning.com>
2019-08-28 20:29:09 +0800

    删除多余日志输出

--- js/waifu-tips.js
+++ js/waifu-tips.js
@@ -76,9 +76,6 @@ function waifuWelcome(){
     var SiteIndexUrl = 'https://dawn1432.github.io/';  // 手动指定主页
 	var SiteIndexUrlDebug = 'http://localhost:4000/' //手动指定调试主页
     //var SiteIndexUrl = window.location.protocol+'//'+window.location.hostname+'/';  // 自动获取主页
-	console.log(window.location.href);
-	console.log(window.location.href == SiteIndexUrl);
-	console.log(window.location.href == SiteIndexUrlDebug);
     if (window.location.href == SiteIndexUrl || window.location.href == SiteIndexUrlDebug) {      // 如果是主页
         var now = (new Date()).getHours();
         if (now > 23 || now <= 5) {
